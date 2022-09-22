558d8a137163add386d1a8108a976a3077581289
TarunBabu <vegitarun1234@gmail.com>
2022-09-22 14:32:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Switch } from 'react-router-dom'
 import Home from './pages/Home'
+import LandingPage from './pages/Home/LandingPage'
 import 'bootstrap/dist/css/bootstrap.css'
 import './pages/Home/styles.css'
 
@@ -8,7 +9,8 @@ function App() {
   return (
     <div className="App">
       <Switch>
-        <Route path='/' component={Home} />
+        <Route path='/' exact component={LandingPage} />
+        <Route path='/getSuggestions' component={Home} />
       </Switch>
     </div>
   );
